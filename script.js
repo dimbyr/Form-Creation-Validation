66d15fc5178e2c719c5782aff2be5e12a502c409
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-07-19 12:05:21 +0300

    Setup and Initial Code Structure

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+
+}
+)
